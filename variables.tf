e7bb79698d692f11b3da6697b1bc91ecbfbf5161
yuulhevv <155242237+yuulhevv@users.noreply.github.com>
2026-03-12 12:26:26 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "prefix" {
+ description = "Префікс для найменування ресурсів (Формат: ім'я-прізвище-варіант)"
+ type = string
+}
+variable "vpc_cidr" {
+ description = "CIDR блок для розгортання VPC"
+ type = string
+ validation {
+ condition = can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/[0-9]{1,2}$", var.vpc_cidr))
+ error_message = "Помилка: Необхідно вказати валідний IPv4 CIDR блок (10.5.0.0/16)."
+ }
+}
+variable "subnet_a_cidr" {
+ type = string
+}
+variable "subnet_b_cidr" {
+ type = string
+}
+variable "web_port" {
+ description = "TCP Порт для доступу до вебсервера Apache"
+ type = number
+ validation {
+ condition = var.web_port >= 1024 && var.web_port <= 65535
+ error_message = "Помилка: Порт повинен знаходитись у безпечному непривілейованому діапазоні 
+(1024-65535)."
+ }
+}
+variable "apache_server_name" {
+ type = string
+}
+variable "apache_doc_root" {
+ type = string
+}
